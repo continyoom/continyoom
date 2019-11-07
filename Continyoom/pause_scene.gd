a5711bd1b46022fded20760f4c4d48b2fa282c24
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2019-11-06 19:40:07 -0800

    Pause button works

--- Continyoom/pause_scene.gd
+++ Continyoom/pause_scene.gd
@@ -11,10 +11,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
+		#de presses action, only important when action is triggered in code
+		Input.action_release("pause")
+		
+		#show/hide when necessary
 		if get_tree().paused:
 			self.hide()
-			
 		if not get_tree().paused:
 			self.show()
-			
+		
+		#switch pause state
 		get_tree().paused = not get_tree().paused
+
+#signal from resume button
+func _on_resume_pressed():
+	Input.action_press("pause")
