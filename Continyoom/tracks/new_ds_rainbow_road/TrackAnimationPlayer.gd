30ba3ace6aacc069f30f7f4186c531dc637dd89a
AlnisS <alnis.smidchens@gmail.com>
2019-11-16 20:09:31 -0800

    animations

--- Continyoom/tracks/new_ds_rainbow_road/TrackAnimationPlayer.gd
+++ Continyoom/tracks/new_ds_rainbow_road/TrackAnimationPlayer.gd
@@ -3,7 +3,7 @@ extends AnimationPlayer
 func _ready():
 	var the_box = get_parent().get_parent().get_node("moving_box/the_box")
 	if (the_box != null):
-		the_box.connect("timescale_update", self, "_on_timescale_update")
+		get_parent().get_parent().get_node("moving_box/the_box").connect("timescale_update", self, "_on_timescale_update")
 
 func _on_timescale_update(var new_timescale):
-	set_speed_scale(new_timescale * 4)
+	set_speed_scale(new_timescale * 1)
