2c11476039465d00887c78d8711c5c3e6fc2e2f7
AlnisS <alnis.smidchens@gmail.com>
2019-11-17 16:10:26 -0800

    complete mute button

--- Continyoom/tracks/new_ds_rainbow_road/AudioStreamPlayer.gd
+++ Continyoom/tracks/new_ds_rainbow_road/AudioStreamPlayer.gd
@@ -2,6 +2,7 @@ extends AudioStreamPlayer
 
 var mute
 const MUTE_SPEED = 4
+var complete_mute = false
 
 func _ready():
 	yield(get_tree().create_timer(.1), "timeout")  # no clue why this works
@@ -20,6 +21,9 @@ func _physics_process(delta):
 		if (mute < 0):
 			mute = 0
 	update_mute()
+	if (Input.is_action_just_pressed("mute")):
+		complete_mute = !complete_mute
+	complete_mute(complete_mute)
 
 func update_mute():
 	var effect = AudioServer.get_bus_effect(AudioServer.get_bus_index("PauseEQ"), 0)
@@ -27,3 +31,6 @@ func update_mute():
 	effect.set_band_gain_db(4, -40 * mute)
 	effect.set_band_gain_db(5, -40 * mute)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -3 * mute)
+
+func complete_mute(var mute):
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), mute)
